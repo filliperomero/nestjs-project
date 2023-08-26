8b460c7981c973a12ec0873ec04022efd61b2673
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-26 16:44:07 -0600

    Add create question controller

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,52 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
+
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { content, title } = body
+    const { sub: userId } = user
+
+    await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        slug: this.convertToSlug(title),
+        authorId: userId,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-') // remove all white spaces
+      .replace(/[^\w-]+/g, '') // remove everything that is not considered words
+      .replace(/_/g, '-') // replace "_" for "-"
+      .replace(/--+/g, '-') //
+      .replace(/-$/g, '') // remove '-' if it is in the end of the string
+  }
+}
