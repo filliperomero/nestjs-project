d6f2a03b7274a23a9596f2bd3f860d0107d94056
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-26 19:23:27 -0600

    Add vitest and paths to typescript

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,9 +2,9 @@ import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { z } from 'zod'
-
-import { Env } from 'src/env'
 import { Injectable } from '@nestjs/common'
+
+import { Env } from '@/env'
 
 const tokenPayloadSchema = z.object({
   sub: z.string().uuid(),
